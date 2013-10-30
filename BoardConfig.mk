6ff294d378ddd9927d5b625a6b23ee727184b402
Nick Kralevich <nnk@google.com>
2013-10-30 14:03:13 -0700

    Move sysfs_devices_system_cpu to the central policy.
    Every device has a CPU. This is not device specific.
    
    Get rid of redundant allow rules, which are already allowed
    by the base policy.
    
    Change-Id: I4766dc571762d8fae06aa8c26828c070b80f5936
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@
 USE_CAMERA_STUB := true
 USE_PROPRIETARY_AUDIO_EXTENSIONS := false
 
-BOARD_HAL_STATIC_LIBRARIES := libdumpstate.grouper
+BOARD_LIB_DUMPSTATE := libdumpstate.grouper
 
 TARGET_RECOVERY_UI_LIB := librecovery_ui_grouper
 
@@ -40,22 +40,5 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/asus/grouper
 -include vendor/asus/grouper/BoardConfigVendor.mk
 include device/asus/grouper/BoardConfigCommon.mk
 
-BOARD_SEPOLICY_DIRS := \
-        device/asus/grouper/sepolicy
+TARGET_RECOVERY_FSTAB = device/asus/grouper/fstab.grouper
 
-BOARD_SEPOLICY_UNION := \
-        file_contexts \
-        genfs_contexts \
-        app.te \
-        btmacreader.te \
-        device.te \
-        drmserver.te \
-        init_shell.te \
-        file.te \
-        rild.te \
-        sensors_config.te \
-        shell.te \
-        surfaceflinger.te \
-        system_app.te \
-        system_server.te \
-        zygote.te
